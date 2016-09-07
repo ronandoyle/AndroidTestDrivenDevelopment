358df7dbdffc35ec6d4bf81965c702516e8fe11e
Ronan <ronandoyle91@gmail.com>
2016-09-07 22:25:54 +0100

    Optimising those imports!

--- app/src/androidTest/java/nanorstudios/ie/testdrivendevelopment/MainActivityInstrumentedTest.java
+++ app/src/androidTest/java/nanorstudios/ie/testdrivendevelopment/MainActivityInstrumentedTest.java
@@ -2,7 +2,6 @@ package nanorstudios.ie.testdrivendevelopment;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
-import android.support.test.espresso.Espresso;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -16,7 +15,7 @@ import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumentation test, which will execute on an Android device.
